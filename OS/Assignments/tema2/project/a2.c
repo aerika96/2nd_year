f5882ea350af458ded4468521482ff2591d6da7e
Albert Erika <erikatimeaalbert@gmail.com>
2017-04-30 12:44:10 +0300

    project at OS added with comments

--- OS/Assignments/tema2/project/a2.c
+++ OS/Assignments/tema2/project/a2.c
@@ -16,46 +16,48 @@ int sem_id;
 int started=0;
 int finished=0;
 int count=0;
+
+//Fuction decrementing a semaphore's value with 1
 void P(int semId, int semNr)
 {
     struct sembuf op = {semNr, -1, 0};
 
     semop(semId, &op, 1);
 }
-void P2(int semId, int semNr)
+
+//Function decrementing the semaphore's value with 4
+void P4(int semId, int semNr)
 {
     struct sembuf op = {semNr, -4, 0};
 
     semop(semId, &op, 1);
 }
-void P43 (int semId, int semNr)
-{
-    struct sembuf op = {semNr, -43,0};
 
-    semop(semId, &op, 1);
-}
+//Function incrementing the semaphore's value with 1
 void V(int semId, int semNr)
 {
     struct sembuf op = {semNr, +1, 0};
 
     semop(semId, &op, 1);
 }
+
+//Function incrementing the semaphore's value with 3
 void V3(int semId, int semNr)
 {
     struct sembuf op = {semNr, +3, 0};
 
     semop(semId, &op, 1);
 }
-void V43(int semId, int semNr)
-{
-    struct sembuf op = {semNr, +43, 0};
 
-    semop(semId, &op, 1);
-}
+//Function for the process nr 3. - synchronizing threads in other processes
 void thread_function3(void*argument){
+
     ThreadT* thr_struc = (ThreadT*)argument;
     int thr = thr_struc->thread;
     int proc = thr_struc->process;
+
+    //thread 3.3 must wait for process 8.3 to terminate before it starts
+    // - semaphore 7 is incremented after thhread 8.3 has finished
     if(thr==3) {
         P(sem_id,7);
         info(BEGIN, proc, thr);
@@ -65,38 +67,43 @@ void thread_function3(void*argument){
         info(BEGIN, proc, thr);
         info(END, proc, thr);
     }
-    if(thr==4){
+    if(thr==4)
+        //thread 8.3 must only start after thread 3.4 has finished
+        //-semaphore 6 incremented after thread 8.4 has terminated
         V(sem_id,6);
-    }
-
 }
 
-void thread_function6(void*argument){
+
+//Function for the process nr 6. - thread barrier
+void thread_function6(void*argument)
+{
     ThreadT* thr_struc = (ThreadT*)argument;
     int thr = thr_struc->thread;
     int proc = thr_struc->process;
 
+    // semaphore 2 has the initial value of 4 - doesn't let more than 4 threads enter
+    // each time a thread tries to enter -tries to decrement the value - incremented after finished
     P(sem_id,2);
     info(BEGIN,proc,thr);
-    V(sem_id,3);
-    P(sem_id,5);
+    V(sem_id,3); //semaphore 3 is incremented each time a thread enters, decremented when has finished - initial value of 0, intended to "count how many threads are currently running"
+    P(sem_id,5); //semaphore 5 's purpose is to "protect" the count global variable's incrementation - has the initial value of 1
     count++;
-    V(sem_id,5);//+1 to end 15
+    V(sem_id,5);
     if(thr==15){
         started=1;
-        P2(sem_id,3);
-       // P2(sem_id, 3);
+        P4(sem_id,3); //when the thread s nr 15. it tries to decrement the semaphore's value with 4 - it means 4 are running - if succeeded, terminates
         info(END, proc, 15);
         finished=1;
-        V3(sem_id,4);
-        V3(sem_id,3);
+        V3(sem_id,4); // semaphore 4 is used to block the termination of all the threads if there are no more than 4 that hasn't been terminated yet
+        V3(sem_id,3); // if thread 15 successfully terminated, it means that it also decremented the semaphore 3's value - has to be incremented, not to block the others
     }
     if(thr!=15){
-
+        //if finished or there were less that 40 threads run -> the others can terminate with no restriction
         if(finished  || count < 40) {
-            P(sem_id, 3);//-1 to end 15
+            P(sem_id, 3);// increments semaphore 3's value
             info(END, proc, thr);
         }
+        //in case there are only 4 threads left and 15 hasn't terminated yet -> all the threads are blocked until it does
         else {
             P(sem_id,4);
             P(sem_id, 3);
@@ -106,19 +113,21 @@ void thread_function6(void*argument){
     V(sem_id,2);
 }
 
+//Function for process nr 8.
 void thread_function8(void *argument)
 {
 
     ThreadT* thr_struc = (ThreadT*)argument;
     int thr = thr_struc->thread;
     int proc = thr_struc->process;
-    if(thr!=1 && thr!=4){
-        if(thr==3) {
 
+    if(thr!=1 && thr!=4){
+        //thread 8.3 has to wait for 3.4 to end - semaphore 6
+        if(thr==3) {
             P(sem_id,6);
             info(BEGIN,proc,thr);
             info(END, proc, thr);
-            V(sem_id,7);
+            V(sem_id,7); // 3.3 must only start after 8.3 has terminated - semaphore 7
         }
         else{
 
@@ -127,7 +136,7 @@ void thread_function8(void *argument)
         }
 
     }
-    else {
+    else { // thread 8.4 has to start before 8.1 but must end only after it
         if(thr==4){
             info(BEGIN,proc,thr);
             V(sem_id,1);
@@ -147,22 +156,26 @@ int main(int argc, char **argv){
     init();
     int childPid,state;
     ThreadT arguments[60];
-    pthread_t th8[4];
-    pthread_t th6[43];
-    pthread_t th3[5];
-    sem_id = semget(IPC_PRIVATE, 8, IPC_CREAT | 0600);
+    pthread_t th8[4]; //threads of process 8
+    pthread_t th6[43]; //threads of process 6
+    pthread_t th3[5]; //threads of process 3
+
+    sem_id = semget(IPC_PRIVATE, 8, IPC_CREAT | 0600); // creating 8 semaphores - the number of semaphores I use in total
     if (sem_id < 0) {
         perror("Error creating the semaphore set");
         exit(2);
     }
+    //initializing semaphores
     semctl(sem_id, 0, SETVAL, 0);//P8
     semctl(sem_id,1,SETVAL,0);//P8
     semctl(sem_id,2,SETVAL,4);//P6
     semctl(sem_id,3,SETVAL,0);//P6
     semctl(sem_id,4,SETVAL,0);//P6
     semctl(sem_id,5,SETVAL,1);//P6
-    semctl(sem_id,6,SETVAL,0);
-    semctl(sem_id,7,SETVAL,0);
+    semctl(sem_id,6,SETVAL,0);//P3,P8
+    semctl(sem_id,7,SETVAL,0);//P3,P8
+
+    //process hierarchy "construction" starts here
     info(BEGIN, 1, 0);
     childPid=fork(); // P1
     switch(childPid) {
